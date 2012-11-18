840bdb9a0fae53f89ed45a5227a691e9ad11dc50
Daniel Margreiter <danielm@live.at>
2012-11-18 21:44:20 +0100

    several attempts to include a logging lib > failed

--- Code/src/Client/Config.h
+++ Code/src/Client/Config.h
@@ -9,17 +9,3 @@
  * common define variables:
  *
  */
-
-
-
-
-
-/*
- * logger
- *
- */
-
-#include "../libs/apache-log4cxx-0.10.0/src/main/include/log4cxx/logger.h"
-#include "../libs/apache-log4cxx-0.10.0/src/main/include/log4cxx/basicconfigurator.h"
-
-using namespace log4cxx;
