0118ff0a6c750510331b2390220cc176c4121a19
Daniel Satanik <daniel.satanik@gmail.com>
2012-11-23 01:54:11 +0100

    join ausformuliert zugehörige klassen erstellt meisten implementierungen durchgeführt

--- Code/src/Server/Main.cpp
+++ Code/src/Server/Main.cpp
@@ -1,9 +1,30 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
+#include <boost\thread\thread.hpp>
+
+#include "network\NetworkManager.h"
+#include "game\Game.h"
 
 using namespace std;
 
 int main(int argc, char argv[])
 {
+	int port;
+	cin >> port;
+
+	if(cin.fail())
+	{
+		cout << "Error: input failes" << endl;
+	}
+
+	Game game;
+	// add game to networkmanager
+	// networkmanager singleton??
+	// access from game object to networkmanager??
+	NetworkManager nm(port);
+	boost::thread networkThread(nm);
+
 	cout << "test";
 	return 0;
 }
