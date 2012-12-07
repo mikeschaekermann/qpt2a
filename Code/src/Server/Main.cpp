b4a06cb15bf62d50123e323bca277e88902d11bd
Daniel Satanik <daniel.satanik@gmail.com>
2012-12-07 15:04:12 +0100

    separated EventCreator.h and BuildingEvent.h in .h and .cpp to resolve cyclic dependencies

--- Code/src/Server/Main.cpp
+++ Code/src/Server/Main.cpp
@@ -1,10 +1,10 @@
 #define _USE_MATH_DEFINES
 
 #include <iostream>
-#include <boost\thread\thread.hpp>
-#include "game\Game.h"
+#include <boost/thread/thread.hpp>
 #include "event/EventQueue.h"
 #include "network/ServerNetworkManager.h"
+#include "game/Game.h"
 //#include "game\Game.h"
 
 using namespace std;
--- Code/src/Server/event/BuildingEvent.h
+++ Code/src/Server/event/BuildingEvent.h
@@ -16,31 +16,9 @@
 class BuildingEvent : public Event
 {
 public:
-	BuildingEvent(double startTime, NetworkManager& manager, Cell& cell, const vector<Player*>& players) :
-	  m_rManager(manager),
-	  m_rCell(cell),
-	  m_rPlayers(players),
-	  Event(startTime, BUILDING_EVENT_DURATION)
-	  { }
+	BuildingEvent(double startTime, NetworkManager& manager, Cell& cell, const vector<Player*>& players);
 
-	virtual void trigger()
-	{
-		m_rCell.completeCell();
-		Player* current = 0;
-		for (vector<Player*>::const_iterator it = m_rPlayers.begin(); it != m_rPlayers.end(); ++it)
-		{
-			CreateCellComplete complete;
-			complete.cellId = m_rCell.getId();
-			complete.endpoint = (*it)->getEndpoint();
-			m_rManager.send(complete);
-
-			if ((*it)->getConstPopulation().find(m_rCell.getId()))
-			{
-				current = *it;
-			}
-		}
-		EventCreator::getInstance().createAttackEvent(m_dDeadTime, true, *current, m_rCell);
-	}
+	virtual void trigger();
 private:
 	NetworkManager& m_rManager;
 	Cell& m_rCell;
