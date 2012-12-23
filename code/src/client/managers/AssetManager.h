484d501e9321564150ccffbcd1c22ca7a8ff195e
Daniel Margreiter <danielm@live.at>
2012-12-24 00:57:32 +0100

    added asset manager and sound player

--- code/src/client/managers/AssetManager.h
+++ code/src/client/managers/AssetManager.h
@@ -1,17 +1,71 @@
 #pragma once
 
-#include <map>
+#include "../assets/ModelBufferData.h"
+#include "../sound/SoundPlayer.h"
+#include "../../common/Config.h"
+
+#include <unordered_map>
 #include <string>
 
+#include "cinder/gl/Texture.h"
+#include "cinder/gl/GlslProg.h"
+#include "cinder/qtime/QuickTime.h"
+#include "cinder/Xml.h"
+#include "cinder/ImageIo.h"
+
+#include "fmod.hpp"
+
+#include "ASSIMP/Importer.hpp"
+#include "ASSIMP/config.h"
+#include "ASSIMP/postprocess.h"
+#include "ASSIMP/scene.h"
+
 using namespace std;
+using namespace cinder::gl;
+using namespace cinder::qtime;
 
 class AssetManager
 {
 public:
-	AssetManager(void);
 	~AssetManager(void);
 
-private:
-	
+	static AssetManager * const getInstance() { return (manager != nullptr ? manager : (manager = new AssetManager())); }
+	void loadAssets(string filePath);
 
+	ModelBufferData const & getModel(string& modelName) const { return modelMap.at(modelName); }
+	
+	Texture const & getTexture(string& textureName) const { return textureMap.at(textureName); }
+	Texture const & getGuiTexture(string& textureName) const { return guiTextureMap.at(textureName); }
+	
+	MovieGl const & getMovie(string& movieName) const { return movieMap.at(movieName); }
+	MovieGl const & getGuiMovie(string& movieName) const { return guiMovieMap.at(movieName); }
+	
+	FMOD::Sound * getSound(string& soundName) const { return soundMap.at(soundName); }
+	FMOD::Sound * getGuiSound(string& soundName) const { return guiSoundMap.at(soundName); }
+	
+	GlslProg const & getShaderProg(string& shaderName) const { return shaderMap.at(shaderName); }
+	
+	void clearGuiAssets();
+	void clearGameAssets();
+	void releaseInstance() { delete manager; }
+
+private:
+	static AssetManager*							manager;
+
+	unordered_map<string, ModelBufferData>			modelMap;
+	unordered_map<string, Texture>					textureMap;
+	unordered_map<string, Texture>					guiTextureMap;
+	unordered_map<string, MovieGl>					movieMap;
+	unordered_map<string, MovieGl>					guiMovieMap;
+	unordered_map<string, FMOD::Sound*>				soundMap;
+	unordered_map<string, FMOD::Sound*>				guiSoundMap;
+	unordered_map<string, GlslProg>					shaderMap;
+
+	AssetManager(void);
+	AssetManager(const AssetManager& manager);
+	const AssetManager& operator=(const AssetManager& manager);
+
+	ModelBufferData createModel(string filePath);
+	string readShaderFile(string filePath);
+	FMOD::Sound* createSound(XmlTree xml);
 };
--- code/src/common/Helper.h
+++ code/src/common/Helper.h
@@ -3,11 +3,12 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <stdarg.h>
 
 using namespace std;
 
-template <class T>
+template<typename T>
 string concatenate(string str, T value)
 {
-	return str + static_cast<ostringstream*>( &(ostringstream() << value) )->str();
+	return (str += static_cast<ostringstream*>( &(ostringstream() << value) )->str());
 }
