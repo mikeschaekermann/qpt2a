f5ce1b9f17747a432e423dade54f29b9f3b10bdf
Mike Schaekermann <m.schaekermann@spontaneousorder.de>
2013-02-17 01:08:41 +0100

    enhancement: finished fog of war

--- code/resources/shader/fogOfWar.frag
+++ code/resources/shader/fogOfWar.frag
@@ -1,25 +1,37 @@
 #version 110
 
 uniform vec2 screenSize;
-
+uniform float maxOpacity;
 uniform int numOfCells;
 
-uniform vec2 centers2D[];
-uniform vec2 radii2D[];
+uniform vec2 centers2D[169];
+uniform float innerRadii2D[169];
+uniform float outerRadii2D[169];
 
 varying vec2 positionNormalized;
 
-float distance1(vec2 a, vec2 b)
-{
-	float diffX = a.x - b.x;
-	float diffY = a.y - b.y;
-
-	return sqrt(diffX * diffX + diffY * diffY);
-}
-
 void main() {	
-	vec2 screenSize1 = vec2(1024., 768.);
+	vec2 screenSize1 = screenSize;
 	vec2 screenPosition = (positionNormalized + vec2(1., 1.)) * 0.5 * screenSize1;
-	
-	gl_FragColor = vec4(0., 0., 0., 0.);
+
+	float maxOpacity2 = 0.5;
+	float opacity = maxOpacity;
+
+	for (int i = 0; i < numOfCells; ++i)
+	{
+		float distance = distance(screenPosition, centers2D[i]);
+
+		if (distance < innerRadii2D[i])
+		{
+			opacity = 0.;
+		}
+		else if (distance < outerRadii2D[i])
+		{
+			float opacityPercentage = (distance - innerRadii2D[i]) / (outerRadii2D[i] - innerRadii2D[i]);
+			float currentOpacity = opacityPercentage * maxOpacity;
+			opacity = min(opacity, currentOpacity);
+		}
+	}
+
+	gl_FragColor = vec4(0., 0., 0., opacity);
 }
--- code/src/client/rendering/RenderManager.cpp
+++ code/src/client/rendering/RenderManager.cpp
@@ -5,12 +5,7 @@
 
 RenderManager* RenderManager::instance = nullptr;
 
-RenderManager::RenderManager():
-	run(true),
-	fogOfWarOpacity(CONFIG_FLOAT2("data.world.fogOfWar.opacity", 0.9)),
-	fogOfWarInnerRadius(CONFIG_FLOAT2("data.world.fogOfWar.innerRadius", 20)),
-	fogOfWarOuterRadius(CONFIG_FLOAT2("data.world.fogOfWar.outerRadius", 20)),
-	fogOfWarSurfaceFront(new Surface())
+RenderManager::RenderManager()
 {
 	auto screenSize = getWindowSize();
 	cam = Cam(screenSize.x, screenSize.y,
@@ -22,33 +17,17 @@ RenderManager::RenderManager():
 		.setPosition(Vec3f( 0.0f, 0.0f, 0.0f ))
 		.setFocus(Vec3f::zero());
 
+	fogOfWarOpacity = CONFIG_FLOAT2("data.world.fogOfWar.opacity", 0.9);
+	fogOfWarInnerRadius = CONFIG_FLOAT2("data.world.fogOfWar.innerRadius", 20);
+	fogOfWarOuterRadius = CONFIG_FLOAT2("data.world.fogOfWar.outerRadius", 20);
+
 	gl::enableAlphaBlending();
 
-	fogOfWarLayer.appendVertex(Vec3f(-1.f,  1.f, -1.f)); /// top left
-	fogOfWarLayer.appendVertex(Vec3f( 1.f,  1.f, -1.f)); /// top right
-	fogOfWarLayer.appendVertex(Vec3f(-1.f, -1.f, -1.f)); /// bottom left
-	fogOfWarLayer.appendVertex(Vec3f( 1.f, -1.f, -1.f)); /// bottom right
-
-	fogOfWarLayer.appendTriangle(0, 2, 1);
-	fogOfWarLayer.appendTriangle(1, 2, 3);
-
-	//fogOfWarThread = boost::thread(boost::bind(&RenderManager::updateFogOfWar, this));
-}
-
-RenderManager::RenderManager(const RenderManager&):
-	run(true),
-	fogOfWarOpacity(CONFIG_FLOAT2("data.world.fogOfWar.opacity", 0.9)),
-	fogOfWarInnerRadius(CONFIG_FLOAT2("data.world.fogOfWar.innerRadius", 20)),
-	fogOfWarOuterRadius(CONFIG_FLOAT2("data.world.fogOfWar.outerRadius", 20)),
-	fogOfWarSurfaceFront(new Surface())
-{
+	initializeFogOfWar();
 }
 
 RenderManager::~RenderManager()
 {
-	run = false;
-
-	fogOfWarThread.join();
 }
 
 RenderManager * const RenderManager::getInstance()
@@ -123,45 +102,7 @@ void RenderManager::setUp3d()
 
 void RenderManager::shutdown3d()
 {
-		auto fogOfWarShaderProgram = ASSET_MGR->getShaderProg(string("fogOfWar"));
-		fogOfWarShaderProgram.bind();
-
-		GAME_SCR.getContainerMutex().lock();
-
-		auto& cellsExploring = GAME_SCR.getExploringCells();
-		int numOfCells = cellsExploring.getSize();
-
-		Vec2f * centers2D = new Vec2f[numOfCells];
-		Vec2f * radii2D = new Vec2f[numOfCells];
-		
-		unsigned int i = 0;
-
-		for (auto it = cellsExploring.begin(); it != cellsExploring.end(); ++it)
-		{
-			auto center3D = it->second->getPosition();
-			auto center2D = cam.worldToScreen(center3D, getWindowWidth(), getWindowHeight());
-			
-			auto radius3D = it->second->getRadius();
-			auto innerRadius2D = center2D.distance(cam.worldToScreen(center3D + Vec3f(radius3D + fogOfWarInnerRadius, 0, 0), getWindowWidth(), getWindowHeight()));
-			auto outerRadius2D = center2D.distance(cam.worldToScreen(center3D + Vec3f(radius3D + fogOfWarInnerRadius + fogOfWarOuterRadius, 0, 0), getWindowWidth(), getWindowHeight()));
-
-			centers2D[i] = center2D;
-			radii2D[i].x = innerRadius2D;
-			radii2D[i].y = outerRadius2D;
-
-			++i;
-		}
-
-		fogOfWarShaderProgram.uniform(string("screenSize"), getWindowSize());
-		fogOfWarShaderProgram.uniform(string("numOfCells"), numOfCells);
-		fogOfWarShaderProgram.uniform(string("centers2D"), centers2D, numOfCells);
-		fogOfWarShaderProgram.uniform(string("radii2D"), radii2D, numOfCells);
-
-		GAME_SCR.getContainerMutex().unlock();
-
-		gl::draw(fogOfWarLayer);
-
-		fogOfWarShaderProgram.unbind();
+		drawFogOfWar();
 
 		gl::disableDepthWrite();
 		gl::disableDepthRead();
@@ -178,178 +119,79 @@ void RenderManager::zoomToWorld()
 	cam.setPosition(Vec3f(0, 0, camDistance)).setFocus(Vec3f::zero());
 }
 
-void RenderManager::generateFogOfWarTexture()
+void RenderManager::initializeFogOfWar()
 {
-	glGenTextures(1, &textureHandle);
-	glBindTexture(GL_TEXTURE_2D, textureHandle);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, getWindowWidth(), getWindowHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-	
-	float border[4] = {1.f, 1.f, 1.f, 1.f};
-	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, border);
+	fogOfWarLayer = TriMesh();
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LESS);
+	fogOfWarLayer.appendVertex(Vec3f(-1.f,  1.f, -1.f)); /// top left
+	fogOfWarLayer.appendVertex(Vec3f( 1.f,  1.f, -1.f)); /// top right
+	fogOfWarLayer.appendVertex(Vec3f(-1.f, -1.f, -1.f)); /// bottom left
+	fogOfWarLayer.appendVertex(Vec3f( 1.f, -1.f, -1.f)); /// bottom right
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	glGenFramebuffers(1, &fbo);
-	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_RGBA, GL_TEXTURE_2D, textureHandle, 0);
-
-	GLenum result = glCheckFramebufferStatus (GL_FRAMEBUFFER);
-	if (GL_FRAMEBUFFER_COMPLETE != result) {
-		cout << "ERROR: Framebuffer is not complete.\n";
-	}
-
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	fogOfWarLayer.appendTriangle(0, 2, 1);
+	fogOfWarLayer.appendTriangle(1, 2, 3);
 }
 
-void RenderManager::updateFogOfWar()
+void RenderManager::drawFogOfWar() const
 {
-	while (run)
+	auto fogOfWarShaderProgram = ASSET_MGR->getShaderProg(string("fogOfWar"));
+	fogOfWarShaderProgram.bind();
+
+	GAME_SCR.getContainerMutex().lock();
+
+	auto& cellsExploring = GAME_SCR.getExploringCells();
+	int numOfCells = cellsExploring.getSize();
+
+	Vec2f * centers2D = new Vec2f[numOfCells];
+	float * innerRadii2D = new float[numOfCells];
+	float * outerRadii2D = new float[numOfCells];
+		
+	int numOfRelevantCells = 0;
+
+	for (auto it = cellsExploring.begin(); it != cellsExploring.end(); ++it)
 	{
-		fogOfWarMutex.lock();
-
-		fogOfWarLayer = TriMesh();
-		Vec3f topLeft, topRight, bottomLeft, bottomRight;
-		cam.getNearClipCoordinates(&topLeft, &topRight, &bottomLeft, &bottomRight);
-
-		fogOfWarLayer.appendVertex(topLeft);
-		fogOfWarLayer.appendVertex(topRight);
-		fogOfWarLayer.appendVertex(bottomLeft);
-		fogOfWarLayer.appendVertex(bottomRight);
-
-		fogOfWarLayer.appendColorRGBA(ColorA(1.f, 0.f, 0.f, 0.5f));
-		fogOfWarLayer.appendColorRGBA(ColorA(1.f, 0.f, 0.f, 0.5f));
-		fogOfWarLayer.appendColorRGBA(ColorA(1.f, 0.f, 0.f, 0.5f));
-		fogOfWarLayer.appendColorRGBA(ColorA(1.f, 0.f, 0.f, 0.5f));
-
-		fogOfWarLayer.appendTriangle(0, 2, 1);
-		fogOfWarLayer.appendTriangle(1, 2, 3);
-
-		fogOfWarMutex.unlock();
-
-		/*
-		GAME_SCR.getContainerMutex().lock();
-
-		auto& cellsExploring = GAME_SCR.getExploringCells();
-		
-		vector<Vec2f> positions;
-		vector<float> innerRadii;
-		vector<float> outerRadii;
-		
-		positions.reserve(cellsExploring.getSize());
-		innerRadii.reserve(cellsExploring.getSize());
-		outerRadii.reserve(cellsExploring.getSize());
-
-		for (auto it = cellsExploring.begin(); it != cellsExploring.end(); ++it)
-		{
-			auto center3D = it->second->getPosition();
-			auto center2D = cam.worldToScreen(center3D, getWindowWidth(), getWindowHeight());
+		auto center3D = it->second->getPosition();
+		auto center2D = cam.worldToScreen(center3D, getWindowWidth(), getWindowHeight());
 			
-			auto radius3D = it->second->getRadius();
-			auto innerRadius2D = center2D.distance(cam.worldToScreen(center3D + Vec3f(radius3D + fogOfWarInnerRadius, 0, 0), getWindowWidth(), getWindowHeight()));
-			auto outerRadius2D = center2D.distance(cam.worldToScreen(center3D + Vec3f(radius3D + fogOfWarInnerRadius + fogOfWarOuterRadius, 0, 0), getWindowWidth(), getWindowHeight()));
-			
-			positions.emplace_back(center2D);
-			innerRadii.emplace_back(innerRadius2D);
-			outerRadii.emplace_back(outerRadius2D);
-		}
+		auto radius3D = it->second->getRadius();
+		auto innerRadius2D = center2D.distance(cam.worldToScreen(center3D + Vec3f(radius3D + fogOfWarInnerRadius, 0, 0), getWindowWidth(), getWindowHeight()));
+		auto outerRadius2D = center2D.distance(cam.worldToScreen(center3D + Vec3f(radius3D + fogOfWarInnerRadius + fogOfWarOuterRadius, 0, 0), getWindowWidth(), getWindowHeight()));
 
-		GAME_SCR.getContainerMutex().unlock();
-		*/
-		/*
-		for (auto it = positionsAndRadii.begin(); it != positionsAndRadii.end(); ++it)
+		auto halfSize = Vec2f(outerRadius2D, outerRadius2D);
+		auto inverseHalfSize = Vec2f(outerRadius2D, - outerRadius2D);
+
+		/// only pass values to the shader
+		/// if they affect the viewport
+		if (
+			getWindowBounds().contains(center2D + halfSize) ||
+			getWindowBounds().contains(center2D - halfSize) ||
+			getWindowBounds().contains(center2D + inverseHalfSize) ||
+			getWindowBounds().contains(center2D - inverseHalfSize)
+		)
 		{
-			auto innerRadiusSq = innerRadius * innerRadius;
-			auto outerRadiusSq = outerRadius * outerRadius;
-			auto radiusDifferenceSq = outerRadiusSq - innerRadiusSq;
-			/// multiplication by 1.1 to avoid sharp
-			/// horizontal and vertical edges
-			auto halfSize = Vec2f(outerRadius * 1.1, outerRadius * 1.1);
-			auto reverseHalfSize = Vec2f(outerRadius * 1.1, - outerRadius * 1.1);
-			auto upperLeft = center2D - halfSize;
-			auto lowerRight = center2D + halfSize;
-			auto upperRight = center2D - reverseHalfSize;
-			auto lowerLeft = center2D + reverseHalfSize;
+			centers2D[numOfRelevantCells] = center2D;
+			innerRadii2D[numOfRelevantCells] = innerRadius2D;
+			outerRadii2D[numOfRelevantCells] = outerRadius2D;
 
-			auto circleArea = Area(upperLeft, lowerRight);
-		
-			if (
-				getWindowBounds().contains(upperLeft) ||
-				getWindowBounds().contains(lowerRight) ||
-				getWindowBounds().contains(upperRight) ||
-				getWindowBounds().contains(lowerLeft)
-			)
-			{
-				auto pixel = fogOfWarSurfaceBack->getIter();
-
-				while( pixel.line() )
-				{
-					while( pixel.pixel() )
-					{
-						auto distanceSq = pixel.getPos().distanceSquared(center2D);
-						if (distanceSq <= innerRadiusSq)
-						{
-							pixel.a() = 0.f;
-						}
-						else if (distanceSq <= outerRadiusSq)
-						{
-							auto opacity = (distanceSq - innerRadiusSq) / radiusDifferenceSq * 255.f;
-							pixel.a() = min<float>(opacity, pixel.a());
-						}
-					}
-				}
-			}
-		}
-		
-
-		fogOfWarMutex.lock();
-
-		auto tmpSurface = fogOfWarSurfaceFront;
-		fogOfWarSurfaceFront = fogOfWarSurfaceBack;
-		delete tmpSurface;
-
-		fogOfWarMutex.unlock();
-		*/
-	}
-}
-
-void RenderManager::shiftFogOfWar(Vec2f shift)
-{
-	auto fogOfWarSurfaceBack = new Surface(getWindowWidth(), getWindowHeight(), false, cinder::SurfaceChannelOrder::RGBA);
-
-	auto it = fogOfWarSurfaceBack->getIter();
-
-	while( it.line() )
-	{
-		while( it.pixel() )
-		{
-			it.r() = it.g() = it.b() = 0.f;
-			it.a() = 255.f;
+			++numOfRelevantCells;
 		}
 	}
 
-	fogOfWarMutex.lock();
+	GAME_SCR.getContainerMutex().unlock();
 
-	fogOfWarSurfaceBack->copyFrom(*fogOfWarSurfaceFront, fogOfWarSurfaceFront->getBounds(), shift);
-	
-	auto tmpSurface = fogOfWarSurfaceFront;
-	fogOfWarSurfaceFront = fogOfWarSurfaceBack;
-	delete tmpSurface;
+	fogOfWarShaderProgram.uniform("screenSize", Vec2f(getWindowWidth(), getWindowHeight()));
+	fogOfWarShaderProgram.uniform("numOfCells", numOfRelevantCells);
+	fogOfWarShaderProgram.uniform("centers2D", centers2D, numOfRelevantCells);
+	fogOfWarShaderProgram.uniform("innerRadii2D", innerRadii2D, numOfRelevantCells);
+	fogOfWarShaderProgram.uniform("outerRadii2D", outerRadii2D, numOfRelevantCells);
+	fogOfWarShaderProgram.uniform("maxOpacity", fogOfWarOpacity);
 
-	fogOfWarMutex.unlock();
+	delete centers2D;
+	delete innerRadii2D;
+	delete outerRadii2D;
+
+	gl::draw(fogOfWarLayer);
+
+	fogOfWarShaderProgram.unbind();
 }
 
-void RenderManager::drawFogOfWar()
-{
-	gl::color(ColorA(1, 1, 1, 0.9));
-
-	fogOfWarMutex.lock();
-
-	gl::draw(gl::Texture(*fogOfWarSurfaceFront), getWindowBounds());
-
-	fogOfWarMutex.unlock();
-}
