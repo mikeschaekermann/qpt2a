6ac144a288eadc8b7c630921ac2b4f8676499168
Daniel Satanik <daniel.satanik@gmail.com>
2013-02-19 23:05:49 +0100

    bug fixed which caused program to use wrong maxcellvalue

--- code/src/common/Cell.cpp
+++ code/src/common/Cell.cpp
@@ -1,9 +1,9 @@
 #include "Cell.h"
 
-Cell::Cell() : maxHealthPoints(1.f), polyMax(CONFIG_INT2("data.polypetide.maxPerCell", 10))
+Cell::Cell() : maxHealthPoints(1.f), polyMax(CONFIG_INT1("data.polypeptide.maxPerCell"))
 { }
 
-Cell::Cell(Vec3f position, float angle, Player * owner) : owner(owner), maxHealthPoints(1.f), polyMax(CONFIG_INT2("data.polypetide.maxPerCell", 10))
+Cell::Cell(Vec3f position, float angle, Player * owner) : owner(owner), maxHealthPoints(1.f), polyMax(CONFIG_INT1("data.polypeptide.maxPerCell"))
 {
  this->position = position;
  setAngle(angle);
@@ -12,7 +12,7 @@ Cell::Cell(Vec3f position, float angle, Player * owner) : owner(owner), maxHealt
 Cell::Cell(Vec3f position, float radius, float angle, float healthPoints) :
  healthPoints(healthPoints),
  maxHealthPoints(healthPoints),
- polyMax(CONFIG_INT2("data.polypetide.maxPerCell", 10))
+ polyMax(CONFIG_INT1("data.polypeptide.maxPerCell"))
 {
  this->position = position;
  this->radius = radius;
@@ -21,7 +21,7 @@ Cell::Cell(Vec3f position, float radius, float angle, float healthPoints) :
 
 Cell::Cell(Vec3f position, float radius, float angle, float healthPoints, Player * owner) :
  healthPoints(healthPoints),
- polyMax(CONFIG_INT2("data.polypetide.maxPerCell", 10)),
+ polyMax(CONFIG_INT1("data.polypeptide.maxPerCell")),
  maxHealthPoints(healthPoints),
  owner(owner)
 {
