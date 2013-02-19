bd4eb2a93bbd62e513a1c05d4141f3bd7a2a89bc
Daniel Satanik <daniel.satanik@gmail.com>
2013-02-19 13:58:15 +0100

    removed create poly when clicked non stemcell

---
+++ code/src/client/actors/PolypeptideClient.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../../common/Polypeptide.h"
+#include "../../client/actors/GameObjectClient.h"
+#include "../../common/ConfigurationDataHandler.h"
+
+class PolypeptideClient :
+	virtual public Polypeptide,
+	virtual public GameObjectClient
+{
+public:
+	PolypeptideClient() { }
+
+	virtual void setPosition(Vec3f position) { GameObject::setPosition(position); }
+	virtual void setRadius(float radius) { GameObject::setRadius(radius); }
+	
+protected:
+	virtual void setRotation(Vec3f rotation) { GameObject::setRotation(rotation); }
+	virtual void setScale(Vec3f scale) { GameObject::setScale(scale); }
+};
